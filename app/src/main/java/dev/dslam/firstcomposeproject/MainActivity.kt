9dff778036e8a5be0552d8a6ea8db663b869e123
Daniyar Slamkul <d.slamkul@aviata.me>
2023-07-15 21:13:06 +0600

    Added instagram profile card

--- app/src/main/java/dev/dslam/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/dev/dslam/firstcomposeproject/MainActivity.kt
@@ -4,51 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import dev.dslam.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import dev.dslam.firstcomposeproject.ui.theme.InstagramProfileCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MultiplicationTable()
-            InstagramProfileCard()
-        }
-    }
-}
-
-@Preview
-@Composable
-fun MultiplicationTable() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        for (i in 1..9) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(1f)
-            ) {
-                for (j in 1..9) {
-                    Box(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .weight(1f)
-                            .border(width = 1.dp, color = Color.DarkGray)
-                            .background(color = if ((i + j) % 2 == 0) Color.Yellow else Color.White),
-                        contentAlignment = Alignment.Center
-                    ) {
-                        Text(text = "${i * j}")
-                    }
+            FirstComposeProjectTheme {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                ) {
+                    InstagramProfileCard()
                 }
             }
         }
--- app/src/main/java/dev/dslam/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/dev/dslam/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,7 +1,11 @@
 package dev.dslam.firstcomposeproject.ui.theme
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -9,24 +13,31 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-@Preview
 @Composable
 fun InstagramProfileCard() {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
+    Card(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(topStart = 4.dp, topEnd = 4.dp),
+        backgroundColor = MaterialTheme.colors.background,
+        border = BorderStroke(1.dp, MaterialTheme.colors.onBackground)
     ) {
-        Box(modifier = Modifier
-            .background(Color.Yellow)
-            .size(50.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(modifier = Modifier
+                .background(Color.Yellow)
+                .size(50.dp))
 
-        TwoBoxes()
-        TwoBoxes()
-        TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+            TwoBoxes()
+        }
     }
+
 }
 
 @Composable
@@ -45,5 +56,25 @@ private fun TwoBoxes() {
                 .background(Color.Magenta)
                 .size(25.dp)
         )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardLight() {
+    FirstComposeProjectTheme(
+        darkTheme = false
+    ) {
+        InstagramProfileCard()
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCardNight() {
+    FirstComposeProjectTheme(
+        darkTheme = true
+    ) {
+        InstagramProfileCard()
     }
 }
