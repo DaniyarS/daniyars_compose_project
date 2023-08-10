21b37ff00891c7f140d6658a70c66cc30f74e4c2
Daniyar Slamkul <d.slamkul@aviata.me>
2023-08-10 21:06:48 +0600

    swipe to dismiss

--- app/src/main/java/dev/dslam/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/dev/dslam/firstcomposeproject/MainActivity.kt
@@ -9,14 +9,21 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.*
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModelProvider
 import dev.dslam.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import dev.dslam.firstcomposeproject.ui.theme.InstagramProfileCard
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.N)
@@ -31,7 +38,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterialApi::class)
 @RequiresApi(Build.VERSION_CODES.N)
 @Composable
 fun Test(viewModel: MainViewModel) {
@@ -42,14 +49,42 @@ fun Test(viewModel: MainViewModel) {
                 .background(MaterialTheme.colors.background)
         ) {
             val models = viewModel.models.observeAsState(listOf())
-            LazyVerticalGrid(cells = GridCells.Fixed(2)) {
-                items(models.value) { model ->
-                    InstagramProfileCard(
-                        model = model,
-                        onFollowedButtonClickListener = {
-                            viewModel.changeFollowingStatus(it)
-                        }
-                    )
+
+            LazyColumn(
+            ) {
+                items(models.value, key = { it.id }) { model ->
+                    val dismissState = rememberDismissState()
+
+                    if (dismissState.isDismissed(DismissDirection.EndToStart)) {
+                        viewModel.delete(model)
+                    }
+
+                    SwipeToDismiss(
+                        state = dismissState, background = {
+                            Box(
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .background(Color.Red.copy(alpha = 0.5f))
+                                    .fillMaxSize(),
+                                contentAlignment = Alignment.CenterEnd
+                            ) {
+                                Text(
+                                    modifier = Modifier.padding(16.dp),
+                                    text = "Delete item",
+                                    color = Color.White,
+                                    fontSize = 24.sp
+                                )
+                            }
+                        },
+                        directions = setOf(DismissDirection.EndToStart)
+                    ) {
+                        InstagramProfileCard(
+                            model = model,
+                            onFollowedButtonClickListener = {
+                                viewModel.changeFollowingStatus(it)
+                            }
+                        )
+                    }
                 }
             }
         }
